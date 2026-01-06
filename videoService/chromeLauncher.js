438d817cabf1c3732c8b9b98ebd0c8bf27e4a805
Sheff2000 <sheffpb@gmail.com>
2026-01-06 15:10:01 +0200

    update lunch chromium

--- videoService/chromeLauncher.js
+++ videoService/chromeLauncher.js
@@ -1,10 +1,14 @@
 // video/chromeLauncher.js
 import { spawn } from 'child_process';
+import { loadDeviceCreds } from '../identity/index.js';
 
-export function launchChrome({ devicePath, flightId, tokenAccess }) {
+export function launchChrome({ devicePath, flightId, flightUrl, tokenAccess }) {
+
+  const creds = loadDeviceCreds();
   const url =
-    `${process.env.FLIGHT_URL}/${flightId}` +
-    `?agentToken=${tokenAccess}` +
+    `${flightUrl}` +
+    `?token=${tokenAccess}` +
+    `&devId=${creds.device_id}` +
     `&camera=${encodeURIComponent(devicePath)}`;
 
   const args = [
@@ -13,9 +17,15 @@ export function launchChrome({ devicePath, flightId, tokenAccess }) {
     '--use-fake-ui-for-media-stream',
     '--disable-gpu',
     '--no-sandbox',
+    '--enable-features=WebRTC-H264WithOpenH264FFmpeg',
+    '--use-fake-ui-for-media-stream',
+    '--allow-file-access-from-files',
     `--user-data-dir=/tmp/chrome_${Date.now()}`,
     url
   ];
+
+//--disable-gpu иногда ломает H.264 на слабых N100
+// если будут чёрные кадры - убрать флаг
 
   return spawn('chromium', args, { stdio: 'ignore' });
 }
